2c8e86912b2b23bed12d5fb582c37f04c0bf6f7a
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-08-21 16:29:34 +0200

    build script: use env variable

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [[ -z $CSGO_DS_DIR ]]; then
+    echo "Error: Variable CSGO_DS_DIR is unset."
+    echo "Use: export CSGO_DS_DIR=(path)"
+    echo "Example: export CSGO_DS_DIR=/c/csgo-server"
+    exit 1
+fi
+
 current_dir=`pwd`
-cd /e/csgo-ds-experiment/csgo/addons/sourcemod/scripting/
+cd $CSGO_DS_DIR/csgo/addons/sourcemod/scripting/
 ./spcomp.exe "$current_dir/knifestats.sp"
 if [ $? -eq 0 ]; then
     mv $current_dir/knifestats.smx ../plugins
